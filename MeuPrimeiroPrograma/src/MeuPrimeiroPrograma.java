c2e3ec09edfa1cd82cc67976273cd5c7eeceabf0
Elton Souza <elton.souza1515@gmail.com>
2021-10-01 21:07:36 -0300

    primeira aula

---
+++ MeuPrimeiroPrograma/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args){
+        System.out.println("Olá Mundo!");
+        String nome = "Elton";
+        int idade = 19;
+
+        System.out.println("Nome: "+nome+", idade: "+idade);
+    }
+}
