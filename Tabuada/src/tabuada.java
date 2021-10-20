33ad51a9730f2885d781a46c923420889f8b3672
Elton Souza <elton.souza1515@gmail.com>
2021-10-20 17:26:55 -0300

    tabuada

--- Tabuada/src/tabuada.java
+++ Tabuada/src/tabuada.java
@@ -1,5 +1,11 @@
 public class tabuada {
     public static void main(String[] args){
-        
+        System.out.println("==== Tabuada ====");
+        for(int i = 1; i <= 10; i++){           
+            for(int x = 1; x <= 10; x++){
+                System.out.println(i+" * "+x+" = "+i*x);
+            }
+            System.out.println("=============");
+        }
     }
 }
