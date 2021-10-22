14d9d1511924a7a06718784597a3c838d994e445
Elton Souza <elton.souza1515@gmail.com>
2021-10-22 17:20:19 -0300

    update

--- Tabuada/src/tabuada.java
+++ Tabuada/src/tabuada.java
@@ -1,11 +1,12 @@
 public class tabuada {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("==== Tabuada ====");
-        for(int i = 1; i <= 10; i++){           
-            for(int x = 1; x <= 10; x++){
-                System.out.println(i+" * "+x+" = "+i*x);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("-- Tabuada do " + i + " --");
+            for (int x = 1; x <= 10; x++) {
+                System.out.println("    "+i + " * " + x + " = " + i * x);
             }
-            System.out.println("=============");
+            System.out.println(" ==================");
         }
     }
 }
