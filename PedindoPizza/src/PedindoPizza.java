a5a607f910afbe23c243a84a1febbb276e3555fc
elton-souza <elton.souza1515@gmail.com>
2021-10-26 21:36:51 -0300

    pedir pizza

---
+++ PedindoPizza/src/PedindoPizza.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PedindoPizza {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo a Pizzaria! \n-- Cardápio -- \nPizzas: \n1 - Pizza Portuguesa \n2 - Moda da Casa \n3 - Pizza 3 Queijos \n4 - Pizza de Catupiri \nRefrigerantes: \n1 - Coca Cola \n2 - Fanta Uva \n3 - Pepsi \n4 - Guaraná");
+        System.out.println("Quantas pizzas e quais sabores você vai querer? ");
+        System.out.println("Sabores: ");
+        System.out.println("Quantidade: ");
+        System.out.println("E refrigerante");
+    }
+}
